b232cb9e7ebedc0837e3adef91500edd3853ae2f
Felix Razikov <frazykov@gmail.com>
2018-12-27 22:12:17 +0200

    Create create_tables_v1.8.sql

---
+++ scripts/sql/create_tables_v1.8.sql
@@ -0,0 +1,212 @@
+CREATE TABLE ingredients
+(
+  ingredient_id SERIAL NOT NULL,
+  ingredient_name VARCHAR NOT NULL,
+  image_url VARCHAR,
+  description VARCHAR,
+  PRIMARY KEY (ingredient_id)
+);
+
+CREATE TABLE restaurants
+(
+  rest_id SERIAL,
+  name VARCHAR NOT NULL,
+  gps_lat FLOAT,
+  gps_lon FLOAT,
+  address VARCHAR NOT NULL,
+  PRIMARY KEY (rest_id)
+);
+
+CREATE TABLE credit_cards
+(
+  id SERIAL,
+  number INT NOT NULL,
+  owner_name VARCHAR NOT NULL,
+  cvc INT NOT NULL,
+  expires_on INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE ingredients_type
+(
+  type INT NOT NULL,
+  ingredient_id SERIAL,
+  PRIMARY KEY (type, ingredient_id),
+  FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id)
+);
+
+CREATE TABLE Restaurants_type
+(
+  type INT NOT NULL,
+  rest_ID INT NOT NULL,
+  PRIMARY KEY (type, rest_ID),
+  FOREIGN KEY (rest_ID) REFERENCES Restaurants(rest_ID)
+);
+
+CREATE TABLE Dishes
+(
+  price INT NOT NULL,
+  description VARCHAR NOT NULL,
+  dish_ID INT NOT NULL,
+  dish_name VARCHAR NOT NULL,
+  image_url VARCHAR NOT NULL,
+  rest_ID INT NOT NULL,
+  PRIMARY KEY (dish_ID),
+  FOREIGN KEY (rest_ID) REFERENCES Restaurants(rest_ID)
+);
+
+CREATE TABLE Tables
+(
+  table_ID INT NOT NULL,
+  rest_ID INT NOT NULL,
+  PRIMARY KEY (table_ID),
+  FOREIGN KEY (rest_ID) REFERENCES Restaurants(rest_ID)
+);
+
+CREATE TABLE Shifts
+(
+  shift_ID INT NOT NULL,
+  starts_at DATE NOT NULL,
+  ends_at DATE NOT NULL,
+  rest_ID INT NOT NULL,
+  PRIMARY KEY (shift_ID),
+  FOREIGN KEY (rest_ID) REFERENCES Restaurants(rest_ID)
+);
+
+CREATE TABLE Stock_ingredients
+(
+  stock_ID INT NOT NULL,
+  quantity INT NOT NULL,
+  ingredient_ID INT NOT NULL,
+  rest_ID INT NOT NULL,
+  PRIMARY KEY (stock_ID, ingredient_ID),
+  FOREIGN KEY (ingredient_ID) REFERENCES Ingredients(ingredient_ID),
+  FOREIGN KEY (rest_ID) REFERENCES Restaurants(rest_ID)
+);
+
+CREATE TABLE Users
+(
+  user_ID INT NOT NULL,
+  email VARCHAR NOT NULL,
+  phone INT NOT NULL,
+  hashed_passwd VARCHAR NOT NULL,
+  name VARCHAR NOT NULL,
+  table_ID INT NOT NULL,
+  ID INT,
+  PRIMARY KEY (user_ID),
+  FOREIGN KEY (table_ID) REFERENCES Tables(table_ID),
+  FOREIGN KEY (ID) REFERENCES Credit_cards(ID)
+);
+
+CREATE TABLE Roles
+(
+  role_ID INT NOT NULL,
+  role_name VARCHAR NOT NULL,
+  user_ID INT,
+  PRIMARY KEY (role_ID, user_ID),
+  FOREIGN KEY (user_ID) REFERENCES Users(user_ID)
+);
+
+CREATE TABLE dish_ingredients
+(
+  quantity INT NOT NULL,
+  ingredient_ID INT NOT NULL,
+  dish_ID INT NOT NULL,
+  PRIMARY KEY (ingredient_ID, dish_ID),
+  FOREIGN KEY (ingredient_ID) REFERENCES Ingredients(ingredient_ID),
+  FOREIGN KEY (dish_ID) REFERENCES Dishes(dish_ID)
+);
+
+CREATE TABLE Dishes_type
+(
+  type INT NOT NULL,
+  dish_ID INT NOT NULL,
+  PRIMARY KEY (type, dish_ID),
+  FOREIGN KEY (dish_ID) REFERENCES Dishes(dish_ID)
+);
+
+CREATE TABLE Employees
+(
+  employee_ID INT NOT NULL,
+  user_ID INT NOT NULL,
+  rest_ID INT NOT NULL,
+  PRIMARY KEY (employee_ID, user_ID),
+  FOREIGN KEY (user_ID) REFERENCES Users(user_ID),
+  FOREIGN KEY (rest_ID) REFERENCES Restaurants(rest_ID)
+);
+
+CREATE TABLE Orders
+(
+  order_ID INT NOT NULL,
+  status VARCHAR NOT NULL,
+  time_received DATE NOT NULL,
+  time_delivered DATE NOT NULL,
+  table_ID INT NOT NULL,
+  rest_ID INT NOT NULL,
+  user_ID INT NOT NULL,
+  PRIMARY KEY (order_ID),
+  FOREIGN KEY (table_ID) REFERENCES Tables(table_ID),
+  FOREIGN KEY (rest_ID) REFERENCES Restaurants(rest_ID),
+  FOREIGN KEY (user_ID) REFERENCES Users(user_ID)
+);
+
+CREATE TABLE Calls_for_waiter
+(
+  reason VARCHAR NOT NULL,
+  call_date DATE NOT NULL,
+  call_ID INT NOT NULL,
+  table_ID INT NOT NULL,
+  rest_ID INT NOT NULL,
+  user_ID INT NOT NULL,
+  PRIMARY KEY (call_ID),
+  FOREIGN KEY (table_ID) REFERENCES Tables(table_ID),
+  FOREIGN KEY (rest_ID) REFERENCES Restaurants(rest_ID),
+  FOREIGN KEY (user_ID) REFERENCES Users(user_ID)
+);
+
+CREATE TABLE Payments
+(
+  date_accepted DATE NOT NULL,
+  amount INT NOT NULL,
+  payment_ID INT NOT NULL,
+  order_ID INT NOT NULL,
+  user_ID INT NOT NULL,
+  rest_ID INT NOT NULL,
+  PRIMARY KEY (payment_ID),
+  FOREIGN KEY (order_ID) REFERENCES Orders(order_ID),
+  FOREIGN KEY (user_ID) REFERENCES Users(user_ID),
+  FOREIGN KEY (rest_ID) REFERENCES Restaurants(rest_ID)
+);
+
+CREATE TABLE Calls_resolved
+(
+  call_resolve_date DATE NOT NULL,
+  call_resolve_ID INT NOT NULL,
+  employee_ID INT NOT NULL,
+  user_ID INT NOT NULL,
+  call_ID INT NOT NULL,
+  PRIMARY KEY (call_resolve_ID),
+  FOREIGN KEY (employee_ID, user_ID) REFERENCES Employees(employee_ID, user_ID),
+  FOREIGN KEY (call_ID) REFERENCES Calls_for_waiter(call_ID)
+);
+
+CREATE TABLE order_items
+(
+  quantity INT NOT NULL,
+  subtotal INT NOT NULL,
+  dish_ID INT NOT NULL,
+  order_ID INT NOT NULL,
+  PRIMARY KEY (dish_ID, order_ID),
+  FOREIGN KEY (dish_ID) REFERENCES Dishes(dish_ID),
+  FOREIGN KEY (order_ID) REFERENCES Orders(order_ID)
+);
+
+CREATE TABLE working
+(
+  employee_ID INT NOT NULL,
+  user_ID INT NOT NULL,
+  shift_ID INT NOT NULL,
+  PRIMARY KEY (employee_ID, user_ID, shift_ID),
+  FOREIGN KEY (employee_ID, user_ID) REFERENCES Employees(employee_ID, user_ID),
+  FOREIGN KEY (shift_ID) REFERENCES Shifts(shift_ID)
+);
